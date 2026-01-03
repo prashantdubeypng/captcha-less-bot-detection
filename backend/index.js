bbc9d712faa19d9df859cefda80fb35ec0ae4e58
prashant dubey <143105656+prashantdubeypng@users.noreply.github.com>
2026-01-03 10:20:30 +0530

    Add CORS support with specific allowed origins
    Implement CORS middleware with allowed origins

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,29 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
-app.use(cors());
+
+const allowedOrigins = [
+  "https://prashantdubeypng.github.io",
+  "http://localhost:5500",
+  "http://127.0.0.1:5500",
+"https://prashantdubeypng.github.io/captcha-less-bot-detection/"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (Postman, curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("CORS not allowed"));
+  },
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 // ----------------------
 // Google Sheets Auth
 // ----------------------
