6bd9ac84818a8226a0fb1014bd940a872f5f5311
prashant dubey <143105656+prashantdubeypng@users.noreply.github.com>
2026-01-03 09:42:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ function endSession() {
         return;
     }
 
-    fetch("http://localhost:3000/collect", {
+    fetch("https://captcha-less-bot-detection-production.up.railway.app/collect", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(buildPayload())
