4e4208822fd5f5030de21fc2a03fd401d9f6f982
prashant dubey <143105656+prashantdubeypng@users.noreply.github.com>
2026-01-03 10:15:10 +0530

    Set body size limits for JSON and URL-encoded data
    Increase JSON and URL-encoded body size limits to 5mb.

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cors());
 // ----------------------
 // Google Sheets Auth
